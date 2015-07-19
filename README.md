eb51dbdfee9b16300f202d6514d2aa4ebf8bd1c5
“Chris <info@chrisbuttery.com>
2015-07-19 15:01:45 +1000

    Added motorised-pinwheeel.js

--- README.md
+++ README.md
@@ -9,7 +9,8 @@ As an introduction to the Arduino and [Johnny-Five](https://github.com/rwaldron/
 5. Project 5: [Servo Mood Indicator](./mood-indicator.js)
 6. Project 6: [Light Theremin](./light-theremin.js)
 7. Project 7: Keyboard Instrument. Variations: [ one](./keyboard-instrument-1.js) & [ two](./keyboard-instrument-2.js)
-8. Project 8: [Digital Hourglass](./digital-hourglass.js)  
+8. Project 8: [Digital Hourglass](./digital-hourglass.js)
+9. Project 9: [Motorised Pinwheel](./motorised-pinwheel.js)  
 ... more to come.
 
 [You can see all the original arduino starter examples here](https://github.com/arduino/Arduino/tree/master/build/shared/examples/10.StarterKit).
---
+++ motorised-pinwheel.js
@@ -0,0 +1,33 @@
+/**
+ * motorised-pinwheel.js
+ * Arduino Starter Kit example converted to johnny-five
+ * Project 9 - Motorised Pinwheel
+ *
+ * Arduino Starter Kit:
+ * http://www.arduino.cc/starterKit
+ *
+ * Examples:
+ * https://github.com/arduino/Arduino/tree/master/build/shared/examples/10.StarterKit
+ */
+
+var five = require('johnny-five');
+var board = new five.Board();
+var switchPin = 2;
+var motorPin = 9;
+
+board.on('ready', function() {
+  var _this = this;
+
+  // create out pins
+  this.pinMode(motorPin, five.Pin.OUTPUT);
+  this.pinMode(switchPin, five.Pin.INPUT);
+
+  /**
+   * Read the diginatl value of `switchPin`.
+   * This will either be 1 (on) or 0 (off)
+   * Write the value to the motor pin
+   */
+  this.digitalRead(switchPin, function(value) {
+    _this.digitalWrite(motorPin, value);
+  });
+});
